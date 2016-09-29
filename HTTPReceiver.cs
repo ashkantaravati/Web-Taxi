29e5c67d536a15ca97cfa656986aad587d11594e
Ashkan Taravati <ashkan_taravati@hotmail.com>
2016-09-29 17:15:00 +0330

    Including Initial Logic to the Repo

---
+++ HTTPReceiver.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleWebBrowser
+{
+    class HTTPReceiver
+    {
+        string Headers;
+        string AgentName;
+        string Response;
+        public HTTPReceiver(string agentName,string headers)
+        {
+            Headers = headers;
+            AgentName = agentName;
+        }
+        public void Receive(string resourceAddress)
+        {
+            WebClient client = new WebClient();
+            // Add a user agent header in case the 
+            // requested URI contains a query.
+
+            client.Headers.Add(AgentName, Headers);
+            
+            Stream data = client.OpenRead(resourceAddress);
+            StreamReader reader = new StreamReader(data);
+            string response = reader.ReadToEnd();
+            
+            data.Close();
+            reader.Close();
+            Response=response;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.Xml;
+
+namespace SimpleWebBrowser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            HTTPReceiver hr = new HTTPReceiver("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
+            hr.Receive(s);
+        }
+    }
+}
